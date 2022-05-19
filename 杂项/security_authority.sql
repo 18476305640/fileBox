ea04d31a95876dacaa243766f8cf9f69aff381f3
Wozhungjie <63949859+18476305640@users.noreply.github.com>
2022-05-19 15:22:09 +0800

    Add files via upload

---
+++ 杂项/security_authority.sql
@@ -0,0 +1,130 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50737
+ Source Host           : localhost:3306
+ Source Schema         : security_authority
+
+ Target Server Type    : MySQL
+ Target Server Version : 50737
+ File Encoding         : 65001
+
+ Date: 19/05/2022 15:21:36
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for persistent_logins
+-- ----------------------------
+DROP TABLE IF EXISTS `persistent_logins`;
+CREATE TABLE `persistent_logins`  (
+  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `series` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of persistent_logins
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `permission_NAME` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
+  `permission_url` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单地址',
+  `parent_id` int(11) NOT NULL DEFAULT 0 COMMENT '父菜单id',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `ROLE_NAME` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  `ROLE_DESC` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色描述',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (6, 'ROLE_ADMIN', '管理员');
+INSERT INTO `sys_role` VALUES (7, 'ROLE_PRODUCT', '产品管理员');
+INSERT INTO `sys_role` VALUES (8, 'ROLE_ORDER', '订单管理员');
+INSERT INTO `sys_role` VALUES (9, 'ROLE_USER', '普通用户');
+
+-- ----------------------------
+-- Table structure for sys_role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_permission`;
+CREATE TABLE `sys_role_permission`  (
+  `RID` int(11) NOT NULL COMMENT '角色编号',
+  `PID` int(11) NOT NULL COMMENT '权限编号',
+  PRIMARY KEY (`RID`, `PID`) USING BTREE,
+  INDEX `FK_Reference_12`(`PID`) USING BTREE,
+  CONSTRAINT `FK_Reference_11` FOREIGN KEY (`RID`) REFERENCES `sys_role` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_Reference_12` FOREIGN KEY (`PID`) REFERENCES `sys_permission` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role_permission
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称',
+  `password` varchar(120) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `status` int(1) NULL DEFAULT 1 COMMENT '1开启0关闭',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (4, 'admin', '$2a$10$WEDIo1LvQ9h2GrnTXh/Wne.NEoSqhWkeDIP7W1zpRfpOcJNJIv6Ze', 1);
+INSERT INTO `sys_user` VALUES (5, 'zhuangjie_order', '$2a$10$zeHjY0n9KgbkRNJAlf4k2eIY6ABXZRJVfd5xIypbuhqd1o4Nf2k/a', 1);
+INSERT INTO `sys_user` VALUES (6, 'zhuangjie_product', '$2a$10$YA.RxNZ.td9sBE7jnuLfauiiWqXcwrkprhMqX3BtBrvyM.L4xHHD6', 1);
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`  (
+  `UID` int(11) NOT NULL COMMENT '用户编号',
+  `RID` int(11) NOT NULL COMMENT '角色编号',
+  PRIMARY KEY (`UID`, `RID`) USING BTREE,
+  INDEX `FK_Reference_10`(`RID`) USING BTREE,
+  CONSTRAINT `FK_Reference_10` FOREIGN KEY (`RID`) REFERENCES `sys_role` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_Reference_9` FOREIGN KEY (`UID`) REFERENCES `sys_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES (4, 6);
+INSERT INTO `sys_user_role` VALUES (6, 7);
+INSERT INTO `sys_user_role` VALUES (5, 8);
+INSERT INTO `sys_user_role` VALUES (4, 9);
+INSERT INTO `sys_user_role` VALUES (5, 9);
+INSERT INTO `sys_user_role` VALUES (6, 9);
+
+SET FOREIGN_KEY_CHECKS = 1;
